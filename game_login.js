2640d309587e713ae0bf3d59782a09206603f8ac
AllenPV <73430174+AllenPV@users.noreply.github.com>
2020-12-05 14:05:02 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -2,7 +2,7 @@ function Sign_in()
 {
 player_name1 = document.getElementById("Username1").value;
 player_name2 = document.getElementById("Username2").value;
-localStorage.setItem("Username1"),player_name1;
-localStorage.setItem("Username2"),player_name2;
-window.location.replace("game_page.html");
+localStorage.setItem("player_name1",player_name1);
+localStorage.setItem("player_name2",player_name2);
+window.location="game_page.html";
 }
--- game_page.js
+++ game_page.js
@@ -1,1 +1,28 @@
-Player_name1 = localStorage.getItem("play")
+Player_name1 = localStorage.getItem("Username1");
+Player_name2 = localStorage.getItem("Username2");
+Player1_score = 0;
+Player2_score = 0;
+document.getElementById("player1_name").innerHTML = Player_name1 + ":";
+document.getElementById("player2_name").innerHTML = Player_name2 + ":";
+document.getElementById("score1").innerHTML = Player1_score;
+document.getElementById("score2").innerHTML = Player2_score;
+document.getElementById("player_question").innerHTML = "Question turn -"+Player_name1;
+document.getElementById("player_answer").innerHTML = "Answer turn -"+ Player_name2;
+function Send()
+{
+    get_word = document.getElementById("Word").value;
+    word = get_word.toLowerCase();
+    console.log("word in lower case = "+word);
+    CharAt1 = word.CharAt1(1);
+    console.log(CharAt1);
+    length_divide_2 = Math.floor(word.length/2);
+    CharAt2 = word.CharAt2(length_divide_2);
+    console.log(CharAt2);
+    length_minus_1 = word.length-1;
+    CharAt3 = word.CharAt3(length_minus_1);
+    remove_CharAt1 = word.replace(CharAt1,"_");
+    remove_CharAt2 = remove_CharAt1.replace(CharAt2,"_");
+    remove_CharAt3 = remove_CharAt2.replace(CharAt3,"_");
+    console.log(CharAt3);
+}
+
